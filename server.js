797ea4e225c9c4a381800a579a3ac0ef2b06569b
n4n0byt3 <shivbo1@hotmail.com>
2023-02-22 13:09:24 +0000

    completed bonus tasks

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const {sequelize} = require("./db")
 
 //TODO: 
 
+app.get('/restaurants/:id' , async (req, res) => {
+let restaurants = await Restaurant.findByPk(req.params.id)
+let menus = await Menu.findByPk(req.params.id)
+let items = await Item.findByPk(req.params.id)
+res.send(restaurants + ', ' + menus + ', ' + items)
+})
+
+
 app.listen(port, () => {
     sequelize.sync()
     console.log("App listening on port " + port)
